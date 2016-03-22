f4dec1a055a02c79042ebc6c9edb983013467352
Narendra Joshi <narendraj9@gmail.com>
2016-03-22 19:57:03 +0530

    Stop tracking custom.el

--- .emacs.d/custom.el
+++
@@ -1,9 +0,0 @@
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Monaco" :foundry "apple" :slant normal :weight normal :height 98 :width normal))))
- '(font-lock-comment-face ((t (:foreground "pale violet red"))))
- '(isearch ((t (:background "blue" :foreground "white"))))
- '(region ((t (:background "dark slate blue")))))
