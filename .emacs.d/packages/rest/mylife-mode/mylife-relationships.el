dddd3c3a0df46d721d7172cca07d4d63642ab14b
Narendra Joshi <narendraj9@gmail.com>
2016-05-08 07:55:56 +0530

    First implementation of scores.
    I have created a tree of integer widget objects that contain
    their respective scores. Whenever the score of a node changes,
    it informs its parent to re-compute its score. This goes on
    until we reach a node whose parent score widget has an id equal
    to nil.

--- .emacs.d/packages/rest/mylife-mode/mylife-relationships.el
+++ .emacs.d/packages/rest/mylife-mode/mylife-relationships.el
@@ -41,59 +41,100 @@ the form. This function expectes `form-file-path` to be an org file."
     ("Completely or extremely" . 9)))
 
 ;;; View
+(defun mylife-widget-score (&optional parent)
+  "Create a widget for keeping score. The :parent-score-wid for the root
+is nil."
+  (let ((wid (widget-create 'number
+                            :tag "Score"
+                            :size 1
+                            :value 5)))
+    (widget-put wid :parent-score-wid parent)
+    wid))
+
+
 (defun mylife-widget-form (object)
   "Create a widget for the whole form."
-  (let ((heading "Relationship Rating Form")
-        (top-level-objects (org-element-map object 'headline
-                             'identity
-                             nil nil
-                             'headline)))
+  (let* ((heading "Relationship Rating Form")
+         (top-level-objects (org-element-map object 'headline
+                              'identity
+                              nil nil
+                              'headline))
+         (total-score-wid (mylife-widget-score nil))
+         (children
+          (mapcar (lambda (top-level-object)
+                    (mylife-widget-top-level top-level-object total-score-wid))
+                  top-level-objects)))
+    (widget-put total-score-wid :child-score-wids children)
     (setq header-line-format heading)
-    (mapcar 'mylife-widget-top-level top-level-objects)))
+    (goto-char 0)))
 
-(defun mylife-widget-top-level (object)
+(defun mylife-widget-top-level (object &optional parent)
   "Create a widget for a top level category."
   (let* ((heading (org-element-property :raw-value object))
         (contents (org-element-contents object))
         (sub-categories (org-element-map  contents
                             'headline
-                          'identity)))
-    (widget-insert (format "\n%s " heading))
-    (widget-create 'integer
-                   :size 1
-                   :value 0)
-    (mapcar 'mylife-widget-sub-category sub-categories)))
+                          'identity))
+        (nil-id (widget-insert (format "\n%s " heading)))
+        (wid (mylife-widget-score parent))
+        (children (mapcar (lambda (sub-category)
+                            (mylife-widget-sub-category sub-category wid))
+                          sub-categories)))
+    (widget-put wid :child-score-wids children)
+    wid))
+    
 
-(defun mylife-widget-sub-category (object)
+(defun mylife-widget-sub-category (object &optional parent)
   "Create a widget for a sub category under the top level category."
-  (let ((heading (org-element-property :raw-value object))
-        (items (org-element-map object 'item 'identity)))
-    (widget-insert (format "\n%s %s\n" heading "Score"))
-    (mapcar 'mylife-widget-item items)))
+  (let* ((heading (org-element-property :raw-value object))
+        (items (org-element-map object 'item 'identity))
+        (nil-id (widget-insert (format "\n%s " heading)))
+        (wid (mylife-widget-score parent))
+        (children (mapcar (lambda (item)
+                            (mylife-widget-item item wid))
+                          items)))
+    (widget-put wid :child-score-wids children)
+    wid))
 
-(defun mylife-widget-item (object)
+(defun mylife-widget-item (object &optional parent)
   "Create a widget for a question item under the sub-category."
   (let* ((bullet (org-element-property :bullet object))
          (paragraph (car (org-element-contents object)))
          (question (car (org-element-contents paragraph))))
     (widget-insert (format "\n%s" bullet))
-    (mylife-widget-question question mylife-options)))
-    
-(defun mylife-widget-question (text options)
+    (mylife-widget-question question mylife-options parent)))
+
+(defun mylife-widget-question (text options &optional parent)
   "A widget to display a question. OPTIONS is an association list
 with choices and their corresponding scores."
+  (message text)
   (widget-insert text)
-  (apply 'widget-create 
-         'radio-button-choice
-         :value 1
-         :notify (lambda (widget &rest ignore)
-                   (message "You selected %s "
-                            (widget-value widget)))
-         (mapcar (lambda (option)
-                   `(item :tag ,(car option)
-                          :value ,(cdr option)))
-                 options)))
+  (let* ((negativep (string-match-p ".*(R)" text))
+         (compute-score (lambda (val)
+                          (if negativep
+                              (- 10 val)
+                            val))))
+    (apply 'widget-create 
+           'radio-button-choice
+           :value 5
+           :parent-score-wid parent
+           :notify (lambda (wid &rest ignore)
+                     (message (format "You selected %d" (widget-value wid)))
+                     (while (and wid (widget-get wid :parent-score-wid))
+                       (let* ((parent-wid (widget-get wid :parent-score-wid))
+                              (children (widget-get parent-wid :child-score-wids))
+                              (child-scores (mapcar 'widget-value children))
+                              (child-count (+ (length children) 0.0))
+                              (average-score (/ (apply '+ child-scores) child-count)))
+                         (widget-value-set parent-wid average-score)
+                         (setq wid parent-wid)))
+                     (widget-setup))
+           (mapcar (lambda (option)
+                     `(item :tag ,(car option)
+                            :value ,(cdr option)))
+                   options))))
 
+              
 ;;; Create widgets using the above functions
 (defun mylife-form-create-widget (form-object)
     "Creates the widgets given a the form object"
