3843a2adb98e90afbb3e04f54f4344375ec41b1f
Narendra Joshi <narendraj9@gmail.com>
2016-06-25 14:35:16 +0530

    Start tracking custom.el

---
+++ .emacs.d/custom.el
@@ -0,0 +1,29 @@
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Monaco" :foundry "apple" :slant normal :weight normal :height 98 :width normal))))
+ '(font-lock-comment-face ((t (:foreground "pale violet red"))))
+ '(helm-candidate-number ((t (:background "goldenrod" :foreground "black"))))
+ '(helm-header-line-left-margin ((t (:background "chocolate" :foreground "black"))))
+ '(helm-selection ((t (:background "steel blue" :distant-foreground "white"))))
+ '(helm-source-header ((t (:background "saddle brown" :foreground "white" :weight bold :height 1.3 :family "Sans Serif"))))
+ '(isearch ((t (:background "blue" :foreground "white"))))
+ '(region ((t (:background "dark slate blue")))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("7a79624c37828e15e1e218ff69a411a63232a9e3b519f9aae5437826910b2e0a" default)))
+ '(safe-local-variable-values
+   (quote
+    ((org-todo-keyword-faces
+      ("DONE" . "green")
+      ("UNFINISHED" . "red"))
+     (org-todo-keyword-faces quote
+                             (("DONE" . "green")
+                              ("UNFINISHED" . "red")))))))
