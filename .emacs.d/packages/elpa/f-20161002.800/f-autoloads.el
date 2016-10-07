caccc7ef9275e130267b7f3020d442e2d3eb0535
Narendra Joshi <narendraj9@gmail.com>
2016-10-07 18:32:38 +0530

    Update Emacs. Install restclient-mode.

---
+++ .emacs.d/packages/elpa/f-20161002.800/f-autoloads.el
@@ -0,0 +1,15 @@
+;;; f-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("f.el") (22518 40653 182606 459000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; f-autoloads.el ends here
