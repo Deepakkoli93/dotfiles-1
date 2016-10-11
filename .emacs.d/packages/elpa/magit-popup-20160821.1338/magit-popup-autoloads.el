f5a2db4b0c3f6ce553ec8448c5287b0f57c97dab
Narendra Joshi <narendraj9@gmail.com>
2016-10-11 13:14:12 +0530

    Update Emacs.

--- .emacs.d/packages/elpa/magit-popup-20160821.1338/magit-popup-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; magit-popup-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("../../../../../.emacs.d/packages/elpa/magit-popup-20160821.1338/magit-popup-autoloads.el"
-;;;;;;  "../../../../../.emacs.d/packages/elpa/magit-popup-20160821.1338/magit-popup-pkg.el"
-;;;;;;  "../../../../../.emacs.d/packages/elpa/magit-popup-20160821.1338/magit-popup.el")
-;;;;;;  (22476 7640 835350 53000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; magit-popup-autoloads.el ends here
